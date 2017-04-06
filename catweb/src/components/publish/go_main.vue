929a116a2dcade66317d841e656d7c987be49d12
yinzhenwei <yinzhenwei@kuaifazs.com>
2017-04-06 18:52:10 +0800

    register 4.6

---
+++ catweb/src/components/publish/go_main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <head-view></head-view>
+  </div>
+</template>
+<script>
+  import HeadView from './head.vue'
+
+  export default{
+      components: {
+          HeadView
+      }
+  }
+</script>
