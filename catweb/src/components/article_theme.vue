8710f7a09df3a510908fcb9f69a2a369da6e9aee
yinwhm12@163.com <yinwhm12@163.com>
2017-04-16 23:32:08 +0800

    主页面 文章等修改 4.16

---
+++ catweb/src/components/article_theme.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-light content-three">
+          <el-row>
+            <el-col>
+              <div class="content-width-head content-font">
+                最新文章<span style="float: right">more>></span>
+                <template>
+                  <el-row>
+                    <el-col>
+                      <div class="title-css content">主题
+
+                      </div>
+                    </el-col>
+                  </el-row>
+                  <el-row>
+                    <el-col>
+                      <div class="content">
+                        内容
+
+                      </div>
+                    </el-col>
+                  </el-row>
+                  <el-row :gutter="20">
+                    <el-col :span="8">作者</el-col>
+                    <el-col :span="4">时间</el-col>
+                    <el-col :span="4">赞</el-col>
+                    <el-col :span="4">收藏</el-col>
+                    <el-col :span="4">评论</el-col>
+                  </el-row>
+                </template>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+  .content-three {
+    height: 600px;
+  }
+
+  .title-css {
+    text-align: center;
+    height: 60px;
+    font-size: 38px;
+  }
+
+  .content {
+    color: #8492A6;
+    font-family: 仿宋;
+  }
+</style>
+
+<script>
+
+  export default{
+    data(){
+      return {}
+    }
+  }
+</script>
---
+++ catweb/src/components/part_two.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content content-second bg-purple-light">
+          <el-row :gutter="15">
+            <el-col :span="12">
+              <div class="divid-height">
+                <el-row>
+                  <el-col>
+                    <div class="content-width-head content-font">
+                      最新资料<span style="float: right"> 更多>></span>
+                      <div>
+                        <el-table
+                          :data="tableData"
+                          :show-header="false"
+                          style="height: 220px">
+                          <el-table-column
+                            prop="title">
+                          </el-table-column>
+                          <el-table-column
+                            prop="date">
+                          </el-table-column>
+                        </el-table>
+                      </div>
+                    </div>
+                  </el-col>
+                </el-row>
+              </div>
+            </el-col>
+            <el-col :span="12">
+              <div class="divid-height">
+                <el-row>
+                  <el-col>
+                    <div class="content-width-head content-font">
+                      热门帖子<span style="float: right"> 更多>></span>
+                      <div>
+                        <el-table
+                          :data="tableData"
+                          :show-header="false"
+                          style="height: 220px">
+                          <el-table-column
+                            prop="title"
+                          >
+                          </el-table-column>
+                          <el-table-column
+                            prop="date"
+                          >
+                          </el-table-column>
+
+                        </el-table>
+                      </div>
+                    </div>
+                  </el-col>
+                </el-row>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+
+  export default{
+    data(){
+      return {
+        tableData: [{
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        }, {
+          title: '2016-05-02',
+          date: '2016-05-02'
+        },
+        ]
+      }
+    }
+  }
+</script>
