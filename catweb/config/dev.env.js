223b3c906fb505975ab7f90dbb7d478ea0b63047
yinwhm12@163.com <yinwhm12@163.com>
2017-05-21 21:12:28 +0800

    5.21  config/ 访问url修改为远程的ip将localhost改为119.29.186.36

--- catweb/config/dev.env.js
+++ catweb/config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  HOST_URL: 'http://localhost:8089/v1',
+  HOST_URL: 'http://119.29.186.36:8089/v1',
   PAGE_SHOW_COUNT: 5,
 })
--- catweb/config/prod.env.js
+++ catweb/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  HOST_URL: 'http://localhost:8089/v1',
+  HOST_URL: 'http://119.29.186.36:8089/v1',
   PAGE_SHOW_COUNT: 5,
 }
